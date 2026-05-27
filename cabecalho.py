e6cb717d763507f628657e25ec4e999e207e339e
morangoacido11 <ratoeirodasilvalindo9590@gmail.com>
2026-05-27 10:15:16 -0300

    v1

---
+++ cabecalho.py
@@ -0,0 +1,10 @@
+###Definido e Chamando função
+
+def exibir_cabecalho():
+    print("=" *  30)
+    print("SISTEMA DE GESTÃO ESCOLAR")
+    print("Versão 1.0 - Desenvolvido no codespaces")
+    print("=" *  30)
+### Chamando a função 
+exibir_cabecalho()
+print("Menu principal")
